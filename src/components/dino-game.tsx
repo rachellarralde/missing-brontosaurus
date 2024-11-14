c883d674cef35c92ed6a88e0a7bd7435feb65802
rachellarralde <47798322+rachellarralde@users.noreply.github.com>
2024-11-13 16:28:16 -0800

    fix compile issues

--- src/components/dino-game.tsx
+++ src/components/dino-game.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useRef, useState, useCallback } from 'react'
 import Image from 'next/image'
 
 const RecordIcon = () => (
