23b3bc94c3c349f0a31c11cbc57eb977f698c594
kevin-- <3334774+kevin--@users.noreply.github.com>
2024-11-20 22:34:40 -0800

    fixup footer and metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,11 +3,11 @@ import type { Metadata } from "next";
 import { defaultFont } from "./typography";
 import "./globals.css";
 import Navbar from "@/components/navbar";
-//import Footer from "@/components/footer";
+import Footer from "@/components/footer";
 
 export const metadata: Metadata = {
   title: "Missing Brontosaurus",
-  description: "Record label",
+  description: "A Record Label by NAIAD",
   icons: {
     icon: [
       {
@@ -30,7 +30,7 @@ export default function RootLayout({
         <main className="w-full flex justify-center">
           {children}
         </main>
-        {/* <Footer /> */}
+        <Footer />
       </body>
     </html>
   );
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,11 +1,13 @@
 export default function Footer() {
   return (
     <footer className="fixed bottom-0 w-full bg-background/80 backdrop-blur-sm border-t border-muted">
-      <div className="max-w-7xl mx-auto px-4 py-4 flex justify-center items-center">
-        <p className="text-sm text-muted-foreground">
-          © {new Date().getFullYear()} Missing Brontosaurus. All rights reserved.
-        </p>
+      <div className="max-w-7xl mx-auto px-4 py-4 flex justify-center items-center gap-4">
+        <span className="text-sm text-muted-foreground">
+          © {new Date().getFullYear()} Missing Brontosaurus.
+        </span>
+        <span>A Label by NAIAD</span>
+        <span className="text-sm text-muted-foreground">Website by RVCHL &amp; sigill</span>
       </div>
-    </footer>
+    </footer >
   )
 } 
