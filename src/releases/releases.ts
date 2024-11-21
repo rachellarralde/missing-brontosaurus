aa4659160b20583e10c521c950454fc74f2e29a1
kevin-- <3334774+kevin--@users.noreply.github.com>
2024-11-20 22:39:17 -0800

    update final Wyvern links

--- src/releases/releases.ts
+++ src/releases/releases.ts
@@ -11,7 +11,7 @@ export const naiadWyvern:ReleaseInfo = {
     artist:"NAIAD",
     title:"Wyvern",
     path:"naiad/wyvern",
-    bandcampEmbed:`<iframe style="border: 0; width: 350px; height: 470px;" src="https://bandcamp.com/EmbeddedPlayer/album=1942691524/size=large/bgcol=333333/linkcol=2ebd35/tracklist=false/transparent=true/" seamless><a href="https://mynameisnaiad.bandcamp.com/album/naiambiance">NAIAMBIANCE by NAIAD</a></iframe>`,
-    buyLink:"https://mynameisnaiad.bandcamp.com/album/wyvern",
+    bandcampEmbed:`<iframe style="border: 0; width: 420px; height: 512px;" src="https://bandcamp.com/EmbeddedPlayer/track=3507047640/size=large/bgcol=333333/linkcol=2ebd35/tracklist=false/transparent=true/" seamless><a href="https://mynameisnaiad.bandcamp.com/track/wyvern">Wyvern by NAIAD</a></iframe>`,
+    buyLink:"https://mynameisnaiad.bandcamp.com/track/wyvern",
     streamLink:"https://ditto.fm/wyvern",
 }
