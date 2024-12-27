272b2e0e50400a3574f9a098dbbe99858b26bf52
kevin-- <3334774+kevin--@users.noreply.github.com>
2024-12-27 12:27:23 -0800

    release NAIAD - Ananconda

--- src/releases/releases.ts
+++ src/releases/releases.ts
@@ -7,11 +7,25 @@ export interface ReleaseInfo {
     streamLink?: string;
 }
 
-export const naiadWyvern:ReleaseInfo = {
-    artist:"NAIAD",
-    title:"Wyvern",
-    path:"naiad/wyvern",
-    bandcampEmbed:`<iframe style="border: 0; width: 420px; height: 512px;" src="https://bandcamp.com/EmbeddedPlayer/track=3507047640/size=large/bgcol=333333/linkcol=2ebd35/tracklist=false/transparent=true/" seamless><a href="https://mynameisnaiad.bandcamp.com/track/wyvern">Wyvern by NAIAD</a></iframe>`,
-    buyLink:"https://mynameisnaiad.bandcamp.com/track/wyvern",
-    streamLink:"https://ditto.fm/wyvern",
+export const naiadWyvern: ReleaseInfo = {
+    artist: "NAIAD",
+    title: "Wyvern",
+    path: "naiad/wyvern",
+    bandcampEmbed: `<iframe style="border: 0; width: 420px; height: 512px;" src="https://bandcamp.com/EmbeddedPlayer/track=3507047640/size=large/bgcol=333333/linkcol=2ebd35/tracklist=false/transparent=true/" seamless><a href="https://mynameisnaiad.bandcamp.com/track/wyvern">Wyvern by NAIAD</a></iframe>`,
+    buyLink: "https://mynameisnaiad.bandcamp.com/track/wyvern",
+    streamLink: "https://ditto.fm/wyvern",
 }
+
+export const naiadAnaconda: ReleaseInfo = {
+    artist: "NAIAD",
+    title: "Anaconda",
+    path: "naiad/anaconda",
+    bandcampEmbed: `<iframe style="border: 0; width: 420px; height: 512px;" src="https://bandcamp.com/EmbeddedPlayer/track=234200418/size=large/bgcol=333333/linkcol=2ebd35/tracklist=false/transparent=true/" seamless><a href="https://mynameisnaiad.bandcamp.com/track/anaconda">Anaconda by NAIAD</a></iframe>`,
+    buyLink: "https://mynameisnaiad.bandcamp.com/track/anaconda",
+    streamLink: "https://ditto.fm/anaconda-naiad"
+}
+
+/** alias to the latest release */
+export const latestRelease = naiadAnaconda;
+/** a list of all releaeses, in reverse chronological order */
+export const allReleases: ReleaseInfo[] = [naiadAnaconda, naiadWyvern];
