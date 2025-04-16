3c0025cde871c36132b3c806500989fc1eb6a2fe
kevin-- <3334774+kevin--@users.noreply.github.com>
2025-04-15 20:42:38 -0700

    add releases route

---
+++ src/app/releases/page.tsx
@@ -0,0 +1,26 @@
+export default function ReleasesPage() {
+    return (
+    //   <div className="min-h-[calc(100vh-8rem)] flex flex-col items-center justify-center p-4">
+    //     <div className="max-w-2xl text-center space-y-6">
+    //       <h1 className="text-4xl font-bold text-foreground mb-8">Missing Brontosaurus</h1>
+          
+    //       <div className="space-y-4 text-muted-foreground">
+    //         <p>
+    //           Independent record label.
+    //         </p>
+            
+    //         <p>
+    //           🚧 Our website is currently under development. 
+    //         </p>
+            
+    //         <p className="text-sm italic">
+    //           More coming soon...
+    //         </p>
+    //       </div>
+    //     </div>
+    //   </div>
+    <div className="flex flex-col items-center justify-center">
+
+    </div>
+    )
+  } 
