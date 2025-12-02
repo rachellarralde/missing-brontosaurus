7004eeecafc2993208b748164ef929e83e8d83a2
kevin-- <3334774+kevin--@users.noreply.github.com>
2025-12-02 15:35:35 -0800

    Change getShowSubmissionsInNav to return true

--- src/lib/environment.ts
+++ src/lib/environment.ts
@@ -3,4 +3,4 @@ export const isDevelopment = () => process.env.NODE_ENV === "development";
 export const getTurnstileKeyDemoSubmissions = () => process.env.NEXT_PUBLIC_TURNSTILE_SITEKEY_DEMO_SUBMISSIONS;
 export const getTurnstileSecretDemoSubmissions = () => process.env.TUNRSTILE_SECRET_DEMO_SUBMISSIONS;
 
-export const getShowSubmissionsInNav = () => false;
+export const getShowSubmissionsInNav = () => true;
